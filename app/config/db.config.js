e063ec5b15a3b18bdcf18007d4bea1b0ff84b5e2
cedric maroka <cedric.maroka@mtn.com>
2023-02-01 23:56:49 +0200

    fix: first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb+srv://amarket:12345@cluster0.0rcg0.mongodb.net/?retryWrites=true&w=majority"
+};
---
+++ app/models/item.model.js
@@ -0,0 +1,25 @@
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+      {
+        title: String,
+        description: String,
+        price: Boolean,
+        dealerId: Number,
+        imgUrls: [String],
+        make: String,
+        model: String,
+        year: String
+
+      },
+      { timestamps: true }
+    );
+  
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+  
+    const Item = mongoose.model("item", schema);
+    return Item;
+  };
