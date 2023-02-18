a95375fb81c937959e009db70bfe8da63020306c
cedric maroka <cedric.maroka@mtn.com>
2023-02-18 22:48:58 +0200

    fix: add code

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
 require("./app/routes/items.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
